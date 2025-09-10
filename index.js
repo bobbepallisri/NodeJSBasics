623f164f3244c38a6e996a0a1edd3226bee01d5b
bobbepallisri <srikanthbobbepalli3@gmail.com>
2025-09-10 21:44:34 +0530

    Add Day1 Node.js practice session

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // using named exports in common js modules.
 
 const calcluator = require('./calcluator')
-import { add, mul, sub } from './calcluator.mjs';
-
 const {add,sub,mul}=calcluator;
 const greeting = (name) =>{
     console.log(`hello, ${name}`)
 }
 
 greeting('srikanth');
-greeting('manasa');
+greeting('chiti');
 console.log(add(2,3));
 console.log(sub(4,2));
 console.log(mul(2,3));
--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-
+// using named exports in ES6 
 import { add, mul, sub } from './calcluator.mjs';
 console.log(add(2,3));
 console.log(sub(4,2));
